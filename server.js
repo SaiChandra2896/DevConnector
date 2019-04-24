e4d2fbb088fefd4fe506ba05c5a632c510ffa7bf
SaiChandra2896 <saichandra2896@gmail.com>
2019-04-24 13:41:39 +0530

    added necessary route files and test them using /test path and imported them to server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
+const user = require('./routes/api/user');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/post');
+
 const app = express();
 
 //DB config
@@ -15,6 +19,12 @@ app.get('/',(req,res) =>{
     res.send('Hello!!');
 });
 
+//use routes
+app.use('/api/user',user);
+app.use('/api/profile',profile);
+app.use('/api/post',post);
+
+
 app.listen(port,() =>{
     console.log('server is up on port '+port);
-})
+});
