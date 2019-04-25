d4b86a37d79747a98de1e15afa3cfbda13f532f7
SaiChandra2896 <saichandra2896@gmail.com>
2019-04-25 15:17:34 +0530

    added a validation for empty name feild

--- validation/registration.js
+++ validation/registration.js
@@ -3,9 +3,15 @@ const isEmpty = require('./isEmpty');
 
 const validateRegisterInput = (data) =>{
    let errors = {};
+
+   data.name = !isEmpty(data.name) ? data.name : '';
+   
    if(!validator.isLength(data.name,{min: 2, max: 30})){
        errors.name = 'Name should be between 2 and 30 charecters';
    };
+   if(validator.isEmpty(data.name)){
+       errors.name = 'Name field is required';
+   }
 
    return {
        errors,
