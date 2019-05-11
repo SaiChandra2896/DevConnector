10b0a61f4797569ddf8e0ffd78b6c55cb3881a30
SaiChandra2896 <saichandra2896@gmail.com>
2019-05-11 23:17:48 +0530

    add route basic to create post

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,8 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-router.get('/test', (req,res) =>{
-    res.json({msg: 'Post works'});
+const Post = require("../../models/Post");
+
+router.get("/test", (req, res) => {
+  res.json({ msg: "Post works" });
 });
 
-module.exports = router
+//create post
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.name,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => {
+      res.json(post);
+    });
+  }
+);
+
+module.exports = router;
