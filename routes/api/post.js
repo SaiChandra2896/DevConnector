68a453414ec17c227182272a7e4ea7ced04ab0cd
SaiChandra2896 <saichandra2896@gmail.com>
2019-05-11 23:32:48 +0530

    add validation to create post route

--- routes/api/post.js
+++ routes/api/post.js
@@ -5,6 +5,9 @@ const passport = require("passport");
 
 const Post = require("../../models/Post");
 
+//validation file
+const validatePostInput = require("../../validation/post");
+
 router.get("/test", (req, res) => {
   res.json({ msg: "Post works" });
 });
@@ -14,10 +17,15 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
-      avatar: req.body.name,
+      avatar: req.body.avatar,
       user: req.user.id
     });
 
