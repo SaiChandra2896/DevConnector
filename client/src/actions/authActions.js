984bd70335c7ce524713ce60310f4baa077271b5
SaiChandra2896 <saichandra2896@gmail.com>
2019-06-21 20:04:48 +0530

    add login using redux store token and set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from './types'
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
 
 //Register user
 
@@ -11,4 +13,34 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     });
+}
+
+//login- get user token
+export const loginUser = (userData) => (dispatch) => {
+  axios.post('/api/user/login', userData).then(res => {
+    //save to local storage
+    const { token } = res.data;
+
+    localStorage.setItem('jwtToken', token);
+    //set token to auth header
+    setAuthToken(token);
+    //decode token to get user data
+    const decoded = jwt_decode(token);
+    //set current user
+    dispatch(setCurrentUser(decoded));
+  }).catch(err => {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  })
+}
+
+//set logged in user
+
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
 }
