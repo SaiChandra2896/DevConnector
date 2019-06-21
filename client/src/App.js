8f49fdc79db40c04bd487a496799baaebc18ed7d
SaiChandra2896 <saichandra2896@gmail.com>
2019-06-21 20:42:20 +0530

    added check for token for every page reload

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store'
 
@@ -11,7 +14,15 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
 
-
+//check for token
+if (localStorage.jwtToken) {
+  //setauthtoken header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
